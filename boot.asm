f425487021bdbdcbe5b51c8556756f7813c2aec9
737 <baramuyu@gmail.com>
2019-02-23 21:23:15 -0800

    new: stack

--- boot.asm
+++ boot.asm
@@ -1,18 +1,34 @@
 ; > nasm -f bin boot.asm -o boot.bin
 ; > qemu-system-i386 boot.bin
 
-[org 0x7c00]
+;[org 0x7c00]
 
-mov ah, 0x0e
+mov ah, 0x0e ; tty mode
+
+mov bp, 0x8000 ;
+mov sp, bp
+
+push 'A'
+push 'B'
+push 'C'
+
+pop bx
+mov al, bl
+int 0x10 ; 'C' [0x7ffa]
+
+pop bx
+mov al, bl
+int 0x10 ; 'B' [0x7ffc]
+
+mov al, [0x7ffe] ; show 'A'
 int 0x10
-mov bx, the_secret
-mov al, [bx]
-int 0x10
+
+pop bx
+mov al, bl
+int 0x10 ; 'A' [0x7ffe]
 
 jmp $ ; jump to current address -> loop
 
-the_secret:
-	db "X"
-
 times 510-($-$$) db 0
 dw 0xaa55
+ 	
