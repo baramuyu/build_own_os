1d2f9117df4c50491c56104cfa36efe3230f8ad8
737 <baramuyu@gmail.com>
2019-02-23 18:11:02 -0800

    new: show 'X'

--- boot.asm
+++ boot.asm
@@ -1,17 +1,18 @@
-;nasm -f bin boot.asm -o boot.bin
+; > nasm -f bin boot.asm -o boot.bin
+; > qemu-system-i386 boot.bin
 
-mov ah, 0x0e; switchtty mode
-mov al, 'H'
+[org 0x7c00]
+
+mov ah, 0x0e
 int 0x10
-mov al, 'e'
-int 0x10
-mov al, 'l'
-int 0x10
-int 0x10 ; repeat 'l'
-mov al, 'o'
+mov bx, the_secret
+mov al, [bx]
 int 0x10
 
 jmp $ ; jump to current address -> loop
+
+the_secret:
+	db "X"
 
 times 510-($-$$) db 0
 dw 0xaa55
