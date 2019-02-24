a4652fd07d0f0770ab170e65bef123edce7239ec
737 <baramuyu@gmail.com>
2019-02-23 17:54:25 -0800

    new: show some letters

--- boot.asm
+++ boot.asm
@@ -1,5 +1,17 @@
-loop:
-	jmp loop
+;nasm -f bin boot.asm -o boot.bin
+
+mov ah, 0x0e; switchtty mode
+mov al, 'H'
+int 0x10
+mov al, 'e'
+int 0x10
+mov al, 'l'
+int 0x10
+int 0x10 ; repeat 'l'
+mov al, 'o'
+int 0x10
+
+jmp $ ; jump to current address -> loop
 
 times 510-($-$$) db 0
 dw 0xaa55
